1cca56935bd75d603d1fcfdb5f06fb372abc8096
Ben North <ben.north@tcd.ie>
2024-03-21 11:36:20 +0000

    Define e2e helpers for front-page version UI

--- cypress/e2e/ui-version-choice.cy.ts
+++ cypress/e2e/ui-version-choice.cy.ts
@@ -8,4 +8,27 @@ context("Choice of UI version", () => {
     cy.pytchResetDatabase({ uiVersion: "v1" });
     cy.contains("try a new way of writing Pytch");
   });
+
+  const goToFrontPage = () =>
+    cy.get(".title-and-version").contains("Pytch").click();
+
+  const getFrontPageChooseV1Link = () =>
+    cy.get(".ToggleUiStylePanel .pseudo-link").contains("back to classic");
+
+  const getFrontPageChooseV2Link = () =>
+    cy.get(".ToggleUiStylePanel .pseudo-link").contains("Try it");
+
+  function assertFrontPageIsV1() {
+    cy.get(".ToggleUiStylePanel").contains("try a new way");
+    getFrontPageChooseV2Link(); // Just check it exists.
+  }
+
+  function assertFrontPageIsV2() {
+    cy.get(".ToggleUiStylePanel").contains("Thanks for trying");
+    getFrontPageChooseV1Link(); // Just check it exists.
+  }
+
+  const chooseFrontPageV1 = () => getFrontPageChooseV1Link().click();
+
+  const chooseFrontPageV2 = () => getFrontPageChooseV2Link().click();
 });
