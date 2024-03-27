aa4f5dcdff2d09b5dc95416f2049232b1c903a33
Ben North <ben.north@tcd.ie>
2024-03-27 14:54:36 +0000

    assertScrollCorrect(): Define local helper

--- cypress/e2e/junior/go-to-error.cy.ts
+++ cypress/e2e/junior/go-to-error.cy.ts
@@ -28,4 +28,30 @@ context("Scroll error-line into view", () => {
   function clickSoleGoToError() {
     cy.get(".go-to-line").should("have.length", 1).click();
   }
+
+  function assertScrollCorrect(scriptIdx: number) {
+    cy.get(".ace_line")
+      .contains("pront")
+      .should("be.visible")
+      .then(($lines) => {
+        const lineDiv = $lines[0] as HTMLElement;
+        const lineBottom = lineDiv.getBoundingClientRect().bottom;
+        cy.get(".AddSomethingButtonStrip").then(($divs) => {
+          const stripDiv = $divs[0] as HTMLElement;
+          const stripTop = stripDiv.getBoundingClientRect().top;
+          const lineOffsetFromBottom = stripTop - lineBottom;
+
+          // The threshold of "20" here is approx
+          // (AceController.kScrollIntoViewLinesBelow × line-stride).
+          cy.wrap(lineOffsetFromBottom).should("be.above", 20);
+
+          if (lineOffsetFromBottom > 25) {
+            cy.get(".PytchScriptEditor")
+              .eq(scriptIdx)
+              .find(".HatBlock")
+              .should("be.visible");
+          }
+        });
+      });
+  }
 });
