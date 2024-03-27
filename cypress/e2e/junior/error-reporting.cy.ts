9c40ae86b42f16a911c71de5e1f8117cf5077398
Ben North <ben.north@tcd.ie>
2024-03-27 14:49:46 +0000

    Simplify test via loadAndRunDemo()

--- cypress/e2e/junior/error-reporting.cy.ts
+++ cypress/e2e/junior/error-reporting.cy.ts
@@ -6,21 +6,11 @@ import {
   selectActorAspect,
   selectInfoPane,
   selectSprite,
+  loadAndRunDemo,
 } from "./utils";
 
 context("Interact with errors", () => {
-  beforeEach(() => {
-    // Initial ** is to match the fetched URL both when running
-    // development server and when serving a deployment zipfile.
-    cy.intercept("GET", "**/cypress/simple-pytchjr-project.zip", {
-      fixture: "project-zipfiles/simple-pytchjr-project.zip",
-    });
-    cy.pytchResetDatabase({
-      initialUrl: "/suggested-demo/cypress/simple-pytchjr-project",
-    });
-    cy.get("button").contains("Demo").click();
-    cy.pytchGreenFlag();
-  });
+  beforeEach(loadAndRunDemo("simple-pytchjr-project"));
 
   type TestFun = (
     program: StructuredProgram,
