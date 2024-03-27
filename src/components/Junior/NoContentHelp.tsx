89430dfb47c0bab2b436d2c44d37935ccd781bbc
Ben North <ben.north@tcd.ie>
2024-03-27 11:27:56 +0000

    NoContentHelp: Add buttonsPlural prop

--- src/components/Junior/NoContentHelp.tsx
+++ src/components/Junior/NoContentHelp.tsx
@@ -4,16 +4,19 @@ import Alert from "react-bootstrap/Alert";
 type NoContentHelpProps = {
   actorKind: string;
   contentKind: string;
+  buttonsPlural: boolean;
 };
 export const NoContentHelp: React.FC<NoContentHelpProps> = ({
   actorKind,
   contentKind,
+  buttonsPlural,
 }) => {
+  const buttonOrButtons = buttonsPlural ? "buttons" : "button";
   return (
     <Alert className="NoContentHelp" variant="primary">
       <p>
-        Your {actorKind} has no {contentKind} yet. Use the button below to add
-        one!
+        Your {actorKind} has no {contentKind} yet. Use the {buttonOrButtons}{" "}
+        below to add one!
       </p>
     </Alert>
   );
