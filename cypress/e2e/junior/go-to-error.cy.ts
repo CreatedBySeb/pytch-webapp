7db4be5e6bc4a8d36f8a9d539c6146fb2f519484
Ben North <ben.north@tcd.ie>
2024-03-27 14:52:48 +0000

    insertNameErrorIntoStage(): Define local helper

--- cypress/e2e/junior/go-to-error.cy.ts
+++ cypress/e2e/junior/go-to-error.cy.ts
@@ -1,5 +1,27 @@
+import { Actions } from "easy-peasy";
+import { StructuredProgram } from "../../../src/model/junior/structured-program";
+import { IActiveProject } from "../../../src/model/project";
 import { loadAndRunDemo } from "./utils";
 
 context("Scroll error-line into view", () => {
   beforeEach(loadAndRunDemo("per-method-long-scripts"));
+
+  function insertNameErrorIntoStage(
+    program: StructuredProgram,
+    scriptIdx: number,
+    lineNo: number,
+    programActions: Actions<IActiveProject>
+  ) {
+    const stage = program.actors[0];
+    const handler = stage.handlers[scriptIdx];
+    let lines = handler.pythonCode.split("\n");
+    const lineIdx = lineNo - 1;
+    lines[lineIdx] = `pront() ${lines[lineIdx]}`;
+    programActions.setHandlerPythonCode({
+      actorId: stage.id,
+      handlerId: handler.id,
+      code: lines.join("\n"),
+    });
+    cy.get(".Junior-CodeEditor").contains("pront");
+  }
 });
