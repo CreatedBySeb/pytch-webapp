05a8ffeba23c3f78026975566e1f7f2b82701c9b
Sébastien Dunne Fulmer <createdbyseb@gmail.com>
2024-04-05 09:53:53 +0100

    Don't block build if resetting the micro:bit fails

--- src/skulpt-connection/build.ts
+++ src/skulpt-connection/build.ts
@@ -69,7 +69,14 @@ export const build = async (
     Sk.pytch.get_active_device = () => {
       return deviceManager.activeDevice;
     };
-    await deviceManager.activeDevice?.reset();
+
+    try {
+      await deviceManager.activeDevice?.reset();
+    } catch (error) {
+      console.error("Error resetting micro:bit, proceeding anyways");
+      console.error(error);
+    }
+
     const codeText = PytchProgramOps.flatCodeText(project.program);
     await Sk.pytchsupport.import_with_auto_configure(codeText);
     Sk.pytch.current_live_project.on_green_flag_clicked();
