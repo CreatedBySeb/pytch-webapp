4ef4431d85cdedb1c5ebeef025a2520e9b617c39
Ben North <ben.north@tcd.ie>
2024-05-01 11:04:51 +0100

    Make fullscreen/standalone-player margin narrower

--- src/constants.ts
+++ src/constants.ts
@@ -4,6 +4,6 @@ export const stageHeight = 360;
 export const stageHalfWidth = 0.5 * stageWidth;
 export const stageHalfHeight = 0.5 * stageHeight;
 
-export const stageFullScreenBorderPx = 16;
+export const stageFullScreenBorderPx = 8;
 
 export const pytchResearchSiteUrl = "https://pytch.scss.tcd.ie/";
