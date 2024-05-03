b685f1f17959cef586ad0cbc9847682d12d31ae6
Ben North <ben.north@tcd.ie>
2024-05-03 11:58:10 +0100

    Add "runs a demo" e2e test

--- cypress/e2e/standalone-play-demo.cy.ts
+++ cypress/e2e/standalone-play-demo.cy.ts
@@ -1,2 +1,14 @@
+import { blueColour, emptyColour } from "./crop-scale-constants";
+import { interceptDemoZipfile } from "./utils";
+
 context("Standalone play-demo", () => {
+  it("runs a demo", () => {
+    interceptDemoZipfile("one-cropped-scaled-sprite");
+    cy.visit("/play-demo/fake-build-id-for-tests/one-cropped-scaled-sprite");
+    cy.pytchCanvasShouldBeSolidColour(blueColour);
+    cy.pytchClickStage(0, 0);
+    cy.pytchCanvasShouldBeSolidColour(emptyColour);
+    cy.pytchGreenFlag();
+    cy.pytchCanvasShouldBeSolidColour(blueColour);
+  });
 });
