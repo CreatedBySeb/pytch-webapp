d6f9fe3484ab7973dcb253cd32c054424950e7a6
Ben North <ben.north@tcd.ie>
2024-05-03 12:54:32 +0100

    useStageDimensionsStyle(): Define local hook

--- src/components/StandalonePlayDemo.tsx
+++ src/components/StandalonePlayDemo.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import { useParams } from "react-router-dom";
 
 import { EmptyProps } from "../utils";
 
 import { useStoreActions, useStoreState } from "../store";
 import { Actions, State } from "easy-peasy";
+
+import { eqDisplaySize } from "../model/ui";
 
 import {
   StandalonePlayDemoState,
@@ -25,6 +27,29 @@ function useSPDActions<R>(mapActions: SPDActionsMapper<R>): R {
     mapActions(actions.standalonePlayDemoState)
   );
 }
+
+////////////////////////////////////////////////////////////////////////
+// Hook for computing width/height styles to match stage dimensions.
+
+type UseStageDimensionsStyleArgs = {
+  includeHeight: boolean;
+};
+
+const useStageDimensionsStyle = ({
+  includeHeight,
+}: UseStageDimensionsStyleArgs): CSSProperties => {
+  const stageDimensions = useStoreState(
+    (state) => state.ideLayout.stageDisplaySize,
+    eqDisplaySize
+  );
+
+  let style: CSSProperties = { width: `${stageDimensions.width}px` };
+  if (includeHeight) {
+    style.height = `${stageDimensions.height}px`;
+  }
+
+  return style;
+};
 
 ////////////////////////////////////////////////////////////////////////
 // Main component.
