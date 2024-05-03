257035e1d803087a48c41fec310f17519d244fc9
Ben North <ben.north@tcd.ie>
2024-05-03 12:53:21 +0100

    Define state/action mapper helpers

--- src/components/StandalonePlayDemo.tsx
+++ src/components/StandalonePlayDemo.tsx
@@ -11,6 +11,22 @@ import {
 } from "../model/standalone-play-demo";
 
 ////////////////////////////////////////////////////////////////////////
+// Utilities for using slice's state/actions.
+
+type SPDStateMapper<R> = (state: State<StandalonePlayDemoState>) => R;
+type SPDActionsMapper<R> = (actions: Actions<StandalonePlayDemoState>) => R;
+
+function useSPDState<R>(mapState: SPDStateMapper<R>): R {
+  return useStoreState((state) => mapState(state.standalonePlayDemoState));
+}
+
+function useSPDActions<R>(mapActions: SPDActionsMapper<R>): R {
+  return useStoreActions((actions) =>
+    mapActions(actions.standalonePlayDemoState)
+  );
+}
+
+////////////////////////////////////////////////////////////////////////
 // Main component.
 
 export const StandalonePlayDemo: React.FC<EmptyProps> = () => {
