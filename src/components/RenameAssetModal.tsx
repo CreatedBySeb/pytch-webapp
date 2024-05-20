c01d91fc37255b64122019ee30a6721f4f613cb0
Ben North <ben.north@tcd.ie>
2024-05-20 21:34:45 +0100

    RenameAssetModal: Port to flow model

--- src/components/RenameAssetModal.tsx
+++ src/components/RenameAssetModal.tsx
@@ -1,41 +1,31 @@
 import React, { useEffect } from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
-import { useStoreActions, useStoreState } from "../store";
 import { MaybeErrorOrSuccessReport } from "./MaybeErrorOrSuccessReport";
-import { focusOrBlurFun } from "../utils";
 import { CompoundTextInput } from "./CompoundTextInput";
 import { FormatSpecifier } from "../model/compound-text-input";
+import {
+  flowFocusOrBlurFun,
+  isInteractable,
+  isSucceeded,
+  maybeLastFailureMessage,
+  settleFunctions,
+} from "../model/user-interactions/async-user-flow";
+import { asyncFlowModal } from "./async-flow-modals/utils";
+import { useFlowActions, useFlowState } from "../model";
 
 export const RenameAssetModal = () => {
-  const {
-    oldStem,
-    fixedSuffix,
-    isActive,
-    isInteractable,
-    attemptSucceeded,
-    maybeLastFailureMessage,
-    inputsReady,
-    attemptArgs,
-  } = useStoreState((state) => state.userConfirmations.renameAssetInteraction);
-
-  const { attempt, dismiss, setNewStem } = useStoreActions(
-    (actions) => actions.userConfirmations.renameAssetInteraction
-  );
+  const { fsmState, isSubmittable } = useFlowState((f) => f.renameAssetFlow);
+  const { setNewStem } = useFlowActions((f) => f.renameAssetFlow);
 
   const inputRef: React.RefObject<HTMLInputElement> = React.createRef();
-  useEffect(focusOrBlurFun(inputRef, isActive, isInteractable));
+  useEffect(flowFocusOrBlurFun(inputRef, fsmState));
 
+  return asyncFlowModal(fsmState, (activeFsmState) => {
+  const { oldStem, fixedSuffix } = activeFsmState.runState;
   const oldBasename = `${oldStem}${fixedSuffix}`;
 
-  const handleClose = () => dismiss();
-  const handleRename = () => attempt(attemptArgs);
-
-  const handleEnterKey = () => {
-    if (inputsReady) {
-      handleRename();
-    }
-  };
+  const settle = settleFunctions(isSubmittable, activeFsmState);
 
   const formatSpecifier: FormatSpecifier = [
     {
@@ -47,39 +37,40 @@ export const RenameAssetModal = () => {
   ];
 
   return (
-    <Modal show={isActive} onHide={handleClose} animation={false} centered>
-      <Modal.Header closeButton={isInteractable}>
+    <Modal show={true} onHide={settle.cancel} animation={false} centered>
+      <Modal.Header closeButton={isInteractable(activeFsmState)}>
         <Modal.Title>Rename “{oldBasename}”</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <CompoundTextInput
           formatSpecifier={formatSpecifier}
           onNewUiFragmentValue={setNewStem}
-          onEnterKey={handleEnterKey}
+          onEnterKey={settle.submit}
           ref={inputRef}
         />
         <MaybeErrorOrSuccessReport
           messageWhenSuccess="Renamed!"
-          attemptSucceeded={attemptSucceeded}
-          maybeLastFailureMessage={maybeLastFailureMessage}
+          attemptSucceeded={isSucceeded(activeFsmState)}
+          maybeLastFailureMessage={maybeLastFailureMessage(activeFsmState)}
         />
       </Modal.Body>
       <Modal.Footer>
         <Button
           disabled={!isInteractable}
           variant="secondary"
-          onClick={handleClose}
+          onClick={settle.cancel}
         >
           Cancel
         </Button>
         <Button
-          disabled={!(isInteractable && inputsReady)}
+          disabled={!isSubmittable}
           variant="primary"
-          onClick={handleRename}
+          onClick={settle.submit}
         >
           Rename
         </Button>
       </Modal.Footer>
     </Modal>
   );
+  });
 };
