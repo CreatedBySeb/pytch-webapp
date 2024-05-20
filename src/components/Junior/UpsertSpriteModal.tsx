d76e71ba169d817e224d1b9e7cb10b375a4d3a2d
Ben North <ben.north@tcd.ie>
2024-05-20 21:09:32 +0100

    (Reformat)

--- src/components/Junior/UpsertSpriteModal.tsx
+++ src/components/Junior/UpsertSpriteModal.tsx
@@ -23,96 +23,98 @@ export const UpsertSpriteModal = () => {
   useEffect(flowFocusOrBlurFun(inputRef, fsmState));
 
   return asyncFlowModal(fsmState, (activeFsmState) => {
-  const { upsertionAction, name, nameValidity } = activeFsmState.runState;
-  const handleNameChange = onChangeFun(setName);
-  const settle = settleFunctions(isSubmittable, activeFsmState);
-  const handleKeyPress = submitOnEnterKeyFun(settle.submit, isSubmittable);
+    const { upsertionAction, name, nameValidity } = activeFsmState.runState;
+    const handleNameChange = onChangeFun(setName);
+    const settle = settleFunctions(isSubmittable, activeFsmState);
+    const handleKeyPress = submitOnEnterKeyFun(settle.submit, isSubmittable);
 
-  const validityContent = (() => {
-    switch (nameValidity.status) {
-      case "valid":
-        // Even though this won't be shown, we need some content for the
-        // <P> to have non-zero height:
-        return <p>OK</p>;
-      case "invalid":
-        return <p>That name cannot be used, because {nameValidity.reason}.</p>;
-      default:
-        return assertNever(nameValidity);
-    }
-  })();
+    const validityContent = (() => {
+      switch (nameValidity.status) {
+        case "valid":
+          // Even though this won't be shown, we need some content for the
+          // <P> to have non-zero height:
+          return <p>OK</p>;
+        case "invalid":
+          return (
+            <p>That name cannot be used, because {nameValidity.reason}.</p>
+          );
+        default:
+          return assertNever(nameValidity);
+      }
+    })();
 
-  const content = (() => {
-    switch (upsertionAction.kind) {
-      case "insert":
-        return {
-          title: <span>Create new sprite</span>,
-          messageWhenSuccess: "Created!",
-        };
-      case "update":
-        return {
-          title: (
-            <span>
-              Rename <em>{upsertionAction.previousName}</em>
-            </span>
-          ),
-          messageWhenSuccess: "Renamed!",
-        };
-      default:
-        return assertNever(upsertionAction);
-    }
-  })();
+    const content = (() => {
+      switch (upsertionAction.kind) {
+        case "insert":
+          return {
+            title: <span>Create new sprite</span>,
+            messageWhenSuccess: "Created!",
+          };
+        case "update":
+          return {
+            title: (
+              <span>
+                Rename <em>{upsertionAction.previousName}</em>
+              </span>
+            ),
+            messageWhenSuccess: "Renamed!",
+          };
+        default:
+          return assertNever(upsertionAction);
+      }
+    })();
 
-  return (
-    <Modal
-      className="UpsertSpriteModal"
-      show={true}
-      onHide={settle.cancel}
-      animation={false}
-      centered
-    >
-      <Modal.Header closeButton={isInteractable(activeFsmState)}>
-        <Modal.Title>{content.title}</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Form>
-          <Form.Control
-            type="text"
-            value={name}
-            onChange={handleNameChange}
-            onKeyDown={handleKeyPress}
-            tabIndex={-1}
-            ref={inputRef}
-          ></Form.Control>
-        </Form>
-        <Alert
-          variant="danger"
-          className={`validity-assessment ${nameValidity.status}`}
-        >
-          {validityContent}
-        </Alert>
-        <MaybeErrorOrSuccessReport
-          messageWhenSuccess={content.messageWhenSuccess}
-          attemptSucceeded={isSucceeded(activeFsmState)}
-          maybeLastFailureMessage={maybeLastFailureMessage(activeFsmState)}
-        />
-      </Modal.Body>
-      <Modal.Footer>
-        <Button
-          disabled={!isInteractable}
-          variant="secondary"
-          onClick={settle.cancel}
-        >
-          Cancel
-        </Button>
-        <Button
-          disabled={!isSubmittable}
-          variant="primary"
-          onClick={settle.submit}
-        >
-          OK
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  );
+    return (
+      <Modal
+        className="UpsertSpriteModal"
+        show={true}
+        onHide={settle.cancel}
+        animation={false}
+        centered
+      >
+        <Modal.Header closeButton={isInteractable(activeFsmState)}>
+          <Modal.Title>{content.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Control
+              type="text"
+              value={name}
+              onChange={handleNameChange}
+              onKeyDown={handleKeyPress}
+              tabIndex={-1}
+              ref={inputRef}
+            ></Form.Control>
+          </Form>
+          <Alert
+            variant="danger"
+            className={`validity-assessment ${nameValidity.status}`}
+          >
+            {validityContent}
+          </Alert>
+          <MaybeErrorOrSuccessReport
+            messageWhenSuccess={content.messageWhenSuccess}
+            attemptSucceeded={isSucceeded(activeFsmState)}
+            maybeLastFailureMessage={maybeLastFailureMessage(activeFsmState)}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            disabled={!isInteractable}
+            variant="secondary"
+            onClick={settle.cancel}
+          >
+            Cancel
+          </Button>
+          <Button
+            disabled={!isSubmittable}
+            variant="primary"
+            onClick={settle.submit}
+          >
+            OK
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
   });
 };
