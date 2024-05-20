c2a6329b14abc29b5e36cfc39669904353261b62
Ben North <ben.north@tcd.ie>
2024-05-20 21:22:15 +0100

    (Reformat)

--- src/components/CreateProjectModal.tsx
+++ src/components/CreateProjectModal.tsx
@@ -44,135 +44,135 @@ export const CreateProjectModal = () => {
   useEffect(flowFocusOrBlurFun(inputRef, fsmState));
 
   return asyncFlowModal(fsmState, (activeFsmState) => {
-  const { name, editorKind, whetherExample } = activeFsmState.runState;
-  const settle = settleFunctions(isSubmittable, activeFsmState);
+    const { name, editorKind, whetherExample } = activeFsmState.runState;
+    const settle = settleFunctions(isSubmittable, activeFsmState);
 
-  const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
-    setName(evt.target.value);
-  };
+    const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+      setName(evt.target.value);
+    };
 
-  const handleKeyPress = submitOnEnterKeyFun(settle.submit, isSubmittable);
+    const handleKeyPress = submitOnEnterKeyFun(settle.submit, isSubmittable);
 
-  const editorKindThumbnail =
-    editorKind === "flat" ? FlatEditorThumbnail : PerMethodEditorThumbnail;
+    const editorKindThumbnail =
+      editorKind === "flat" ? FlatEditorThumbnail : PerMethodEditorThumbnail;
 
-  const mEditingModeContent = activeUiVersion === "v2" && (
-    <>
-      <hr />
-      <Form.Group className="editor-kind">
-        <div className="option-buttons">
-          <EditorKindOption
-            thisOption="per-method"
-            activeOption={editorKind}
-            label="Edit as sprites and scripts"
-            setActive={setEditorKind}
-          />
-          <EditorKindOption
-            thisOption="flat"
-            activeOption={editorKind}
-            label="Edit as one big program"
-            setActive={setEditorKind}
-          />
-        </div>
-        <div className="editor-thumbnail">
-          <img src={editorKindThumbnail} />
-        </div>
-      </Form.Group>
-    </>
-  );
-
-  const wrapUiStyleText = (text: string, onClick: () => void) => (
-    <p className="change-ui-style">
-      <span onClick={onClick}>{text}</span>
-    </p>
-  );
-
-  // Ensure that "back to classic Pytch" forces "flat" project:
-  const setUiV1 = () => {
-    setActiveUiVersion("v1");
-    setEditorKind("flat");
-  };
-
-  // And that "try new Pytch" defaults to "per-method" project:
-  const setUiV2 = () => {
-    setActiveUiVersion("v2");
-    setEditorKind("per-method");
-  };
-
-  const changeUiStyleLink =
-    activeUiVersion === "v1"
-      ? wrapUiStyleText("Try our new script-by-script editor!", setUiV2)
-      : wrapUiStyleText("Go back to classic Pytch", setUiV1);
-
-  return (
-    <Modal
-      className="CreateProjectModal"
-      show={isActive(activeFsmState)}
-      onHide={settle.cancel}
-      animation={false}
-      size="lg"
-    >
-      <Modal.Header>
-        <Modal.Title>Create a new project</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Form>
-          <Form.Group>
-            <Form.Control
-              readOnly={!isInteractable(activeFsmState)}
-              type="text"
-              value={name}
-              onChange={handleChange}
-              onKeyDown={handleKeyPress}
-              placeholder="Name for your new project"
-              tabIndex={-1}
-              ref={inputRef}
+    const mEditingModeContent = activeUiVersion === "v2" && (
+      <>
+        <hr />
+        <Form.Group className="editor-kind">
+          <div className="option-buttons">
+            <EditorKindOption
+              thisOption="per-method"
+              activeOption={editorKind}
+              label="Edit as sprites and scripts"
+              setActive={setEditorKind}
             />
-          </Form.Group>
-          <hr />
-          <Form.Group className="whether-include-example">
-            <div className="option-buttons">
-              <WhetherExampleOption
-                thisOption="without-example"
-                activeOption={whetherExample}
-                label="Without example code"
-                setActive={setWhetherExample}
+            <EditorKindOption
+              thisOption="flat"
+              activeOption={editorKind}
+              label="Edit as one big program"
+              setActive={setEditorKind}
+            />
+          </div>
+          <div className="editor-thumbnail">
+            <img src={editorKindThumbnail} />
+          </div>
+        </Form.Group>
+      </>
+    );
+
+    const wrapUiStyleText = (text: string, onClick: () => void) => (
+      <p className="change-ui-style">
+        <span onClick={onClick}>{text}</span>
+      </p>
+    );
+
+    // Ensure that "back to classic Pytch" forces "flat" project:
+    const setUiV1 = () => {
+      setActiveUiVersion("v1");
+      setEditorKind("flat");
+    };
+
+    // And that "try new Pytch" defaults to "per-method" project:
+    const setUiV2 = () => {
+      setActiveUiVersion("v2");
+      setEditorKind("per-method");
+    };
+
+    const changeUiStyleLink =
+      activeUiVersion === "v1"
+        ? wrapUiStyleText("Try our new script-by-script editor!", setUiV2)
+        : wrapUiStyleText("Go back to classic Pytch", setUiV1);
+
+    return (
+      <Modal
+        className="CreateProjectModal"
+        show={isActive(activeFsmState)}
+        onHide={settle.cancel}
+        animation={false}
+        size="lg"
+      >
+        <Modal.Header>
+          <Modal.Title>Create a new project</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group>
+              <Form.Control
+                readOnly={!isInteractable(activeFsmState)}
+                type="text"
+                value={name}
+                onChange={handleChange}
+                onKeyDown={handleKeyPress}
+                placeholder="Name for your new project"
+                tabIndex={-1}
+                ref={inputRef}
               />
-              <WhetherExampleOption
-                thisOption="with-example"
-                activeOption={whetherExample}
-                label="With example code"
-                setActive={setWhetherExample}
-              />
-            </div>
-          </Form.Group>
-          {mEditingModeContent}
-        </Form>
-        <MaybeErrorOrSuccessReport
-          messageWhenSuccess="Project created!"
-          attemptSucceeded={isSucceeded(activeFsmState)}
-          maybeLastFailureMessage={maybeLastFailureMessage(activeFsmState)}
-        />
-      </Modal.Body>
-      <Modal.Footer>
-        {changeUiStyleLink}
-        <Button
-          variant="secondary"
-          onClick={settle.cancel}
-          disabled={!isInteractable}
-        >
-          Cancel
-        </Button>
-        <Button
-          disabled={!isSubmittable}
-          variant="primary"
-          onClick={settle.submit}
-        >
-          Create project
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  );
+            </Form.Group>
+            <hr />
+            <Form.Group className="whether-include-example">
+              <div className="option-buttons">
+                <WhetherExampleOption
+                  thisOption="without-example"
+                  activeOption={whetherExample}
+                  label="Without example code"
+                  setActive={setWhetherExample}
+                />
+                <WhetherExampleOption
+                  thisOption="with-example"
+                  activeOption={whetherExample}
+                  label="With example code"
+                  setActive={setWhetherExample}
+                />
+              </div>
+            </Form.Group>
+            {mEditingModeContent}
+          </Form>
+          <MaybeErrorOrSuccessReport
+            messageWhenSuccess="Project created!"
+            attemptSucceeded={isSucceeded(activeFsmState)}
+            maybeLastFailureMessage={maybeLastFailureMessage(activeFsmState)}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          {changeUiStyleLink}
+          <Button
+            variant="secondary"
+            onClick={settle.cancel}
+            disabled={!isInteractable}
+          >
+            Cancel
+          </Button>
+          <Button
+            disabled={!isSubmittable}
+            variant="primary"
+            onClick={settle.submit}
+          >
+            Create project
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
   });
 };
 
