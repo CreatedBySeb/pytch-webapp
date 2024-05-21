632309644f8dcaabbe8d5a2ee99f86d40710d9c9
Ben North <ben.north@tcd.ie>
2024-05-21 04:28:59 +0100

    FileFailureError: Define type

--- src/model/user-interactions/process-files.ts
+++ src/model/user-interactions/process-files.ts
@@ -6,6 +6,12 @@ export type FileProcessingFailure = {
   reason: string;
 };
 
+export class FileFailureError extends Error {
+  constructor(readonly fileFailures: Array<FileProcessingFailure>) {
+    super("There was a problem when processing a file or files");
+  }
+}
+
 type ScalarState =
   | { status: "idle" }
   | { status: "awaiting-user-choice" }
