42dfaf44c9a1933c1e1a03d163d97903216cb57b
Ben North <ben.north@tcd.ie>
2024-05-21 04:23:31 +0100

    UpsertSpriteFlow: Define model slice
    Replacing UpsertSpriteInteraction.

--- src/model/junior/upsert-sprite.ts
+++ src/model/junior/upsert-sprite.ts
@@ -1,132 +1,114 @@
-import { action, Action, thunk, Thunk } from "easy-peasy";
-import { PytchAppModelActions } from "..";
-
-import {
-  IModalUserInteraction,
-  modalUserInteraction,
-} from "../user-interactions";
+import { Action } from "easy-peasy";
+import { IPytchAppModel, PytchAppModelActions } from "..";
 import {
   NameValidity,
   nameValidity,
   unusedSpriteName,
 } from "./structured-program/name-validity";
-import { assertNever, propSetterAction } from "../../utils";
+import { assertNever } from "../../utils";
 import {
   SpriteUpsertionAction,
   SpriteUpsertionArgs,
 } from "./structured-program/program";
+import {
+  asyncUserFlowSlice,
+  AsyncUserFlowSlice,
+  runStateAction,
+} from "../user-interactions/async-user-flow";
 
-type UpsertSpriteLaunchArgs = {
+type UpsertSpriteRunArgs = {
   upsertionAction: SpriteUpsertionAction;
   existingNames: Array<string>;
 };
 
-type UpsertSpriteBase = IModalUserInteraction<SpriteUpsertionArgs>;
-
-type UpsertSpriteSpecific = {
-  upsertionArgs: SpriteUpsertionArgs;
-  setUpsertionArgs: Action<UpsertSpriteSpecific, SpriteUpsertionArgs>;
-
-  _setName: Action<UpsertSpriteSpecific, string>;
-  setName: Thunk<UpsertSpriteSpecific, string>;
-
-  existingNames: Array<string>;
-  setExistingNames: Action<UpsertSpriteSpecific, Array<string>>;
-
+type UpsertSpriteRunState = UpsertSpriteRunArgs & {
+  name: string;
   nameValidity: NameValidity;
-  launch: Thunk<
-    UpsertSpriteBase & UpsertSpriteSpecific,
-    UpsertSpriteLaunchArgs
-  >;
-  refreshInputsReady: Action<UpsertSpriteBase & UpsertSpriteSpecific>;
 };
 
-const upsertSpriteSpecific: UpsertSpriteSpecific = {
-  upsertionArgs: { kind: "insert", name: "" },
-  setUpsertionArgs: propSetterAction("upsertionArgs"),
+type UpsertSpriteBase = AsyncUserFlowSlice<
+  IPytchAppModel,
+  UpsertSpriteRunArgs,
+  UpsertSpriteRunState
+>;
 
-  _setName: action((state, name) => {
-    state.upsertionArgs.name = name;
-  }),
-  setName: thunk((actions, name) => {
-    actions._setName(name);
-    actions.refreshInputsReady();
-  }),
+type SAction<ArgT> = Action<UpsertSpriteBase, ArgT>;
 
-  // The setter is only called from launch() and followed by setName()
-  // so can leave nameValidity computation to setName().
-  existingNames: [],
-  setExistingNames: propSetterAction("existingNames"),
+type UpsertSpriteActions = {
+  setName: SAction<string>;
+};
 
-  nameValidity: nameValidity([], ""),
+export type UpsertSpriteFlow = UpsertSpriteBase & UpsertSpriteActions;
 
-  launch: thunk((actions, { upsertionAction, existingNames }) => {
-    // Ugh, sequence of actions here is brittle: superLaunch() sets
-    // inputsReady to false; setName() calls refreshInputsReady(), which
-    // refers to existingNames to update nameValidity and hence
-    // inputsReady.
-    actions.superLaunch();
-
-    // This is a bit clunky; we set name to "" here, then overwrite
-    // in the switch(){} below.
-    actions.setUpsertionArgs({ ...upsertionAction, name: "" });
-
-    switch (upsertionAction.kind) {
-      case "insert":
-        actions.setExistingNames(existingNames);
-        actions.setName(unusedSpriteName(existingNames));
-        break;
-      case "update": {
-        // We don't want the dialog to chide the user about "there's
-        // already a sprite with that name" when they haven't changed
-        // the default yet, so pretend the current name is not one of
-        // the existing names.  We separately catch the case that they
-        // haven't changed the default in refreshInputsReady() below.
-        const previousName = upsertionAction.previousName;
-        const disallowedNames = existingNames.filter((n) => n !== previousName);
-        actions.setExistingNames(disallowedNames);
-        actions.setName(previousName);
-        break;
-      }
-      default:
-        assertNever(upsertionAction);
+async function prepare(
+  args: UpsertSpriteRunArgs
+): Promise<UpsertSpriteRunState> {
+  // Assigned in each arm of the switch() below:
+  let name: string;
+  let existingNames: Array<string>;
+  switch (args.upsertionAction.kind) {
+    case "insert":
+      existingNames = args.existingNames;
+      name = unusedSpriteName(existingNames);
+      break;
+    case "update": {
+      // We don't want the dialog to chide the user about "there's
+      // already a sprite with that name" when they haven't changed
+      // the default yet, so pretend the current name is not one of
+      // the existing names.  We separately catch the case that they
+      // haven't changed the default in isSubmittable() below.
+      const previousName = args.upsertionAction.previousName;
+      existingNames = args.existingNames.filter((n) => n !== previousName);
+      name = previousName;
+      break;
     }
-  }),
+    default:
+      // Control-flow analysis re whether existingNames and name are
+      // assigned requires the "throw".
+      throw assertNever(args.upsertionAction);
+  }
 
-  refreshInputsReady: action((state) => {
-    state.nameValidity = nameValidity(
-      state.existingNames,
-      state.upsertionArgs.name
-    );
+  return {
+    upsertionAction: args.upsertionAction,
+    existingNames,
+    name,
+    nameValidity: nameValidity(existingNames, name),
+  };
+}
 
-    const args = state.upsertionArgs;
+function isSubmittable(runState: UpsertSpriteRunState): boolean {
+  const renamingButUnchanged =
+    runState.upsertionAction.kind === "update" &&
+    runState.name === runState.upsertionAction.previousName;
 
-    // If renaming, forbid leaving the name at its previous value:
-    const renamingButUnchanged =
-      args.kind === "update" && args.name === args.previousName;
+  return runState.nameValidity.status === "valid" && !renamingButUnchanged;
+}
 
-    state.inputsReady =
-      state.nameValidity.status === "valid" && !renamingButUnchanged;
-  }),
-};
+async function attempt(
+  runState: UpsertSpriteRunState,
+  actions: PytchAppModelActions
+): Promise<void> {
+  const upsertionArgs: SpriteUpsertionArgs = {
+    ...runState.upsertionAction,
+    name: runState.name,
+  };
 
-const attemptUpsertSprite = async (
-  actions: PytchAppModelActions,
-  descriptor: SpriteUpsertionArgs
-) => {
   // This can throw if name already exists, even though we've tried to
   // not let that happen:
-  const spriteId = actions.activeProject.upsertSprite(descriptor);
+  const spriteId = actions.activeProject.upsertSprite(upsertionArgs);
 
   // The aim here is to focus a newly-added Sprite, but it does no harm
   // to always setFocusedActor(), because for a rename, that Sprite was
   // focused anyway.
   actions.jrEditState.setFocusedActor(spriteId);
-};
+}
 
-export type UpsertSpriteInteraction = UpsertSpriteBase & UpsertSpriteSpecific;
-
-export let upsertSpriteInteraction = modalUserInteraction(
-  attemptUpsertSprite,
-  upsertSpriteSpecific
-);
+export let upsertSpriteFlow: UpsertSpriteFlow = (() => {
+  const specificSlice: UpsertSpriteActions = {
+    setName: runStateAction((state, name) => {
+      state.name = name;
+      state.nameValidity = nameValidity(state.existingNames, name);
+    }),
+  };
+  return asyncUserFlowSlice(specificSlice, prepare, isSubmittable, attempt);
+})();
