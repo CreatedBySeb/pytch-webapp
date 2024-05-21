3577d512cce4d60f68dbf91ded7cec061a27fa8f
Ben North <ben.north@tcd.ie>
2024-05-21 04:28:34 +0100

    IProcessFilesInteraction: Remove (and related)

--- src/model/user-interactions/process-files.ts
+++ src/model/user-interactions/process-files.ts
@@ -1,6 +1,3 @@
-import { action, Action, thunk, Thunk } from "easy-peasy";
-import { IPytchAppModel } from "..";
-
 export type FileProcessingFailure = {
   fileName: string;
   reason: string;
@@ -11,59 +8,3 @@ export class FileFailureError extends Error {
     super("There was a problem when processing a file or files");
   }
 }
-
-type ScalarState =
-  | { status: "idle" }
-  | { status: "awaiting-user-choice" }
-  | { status: "trying-to-process" };
-
-type ScalarStatus = ScalarState["status"];
-
-type State =
-  | ScalarState
-  | { status: "showing-failures"; failures: Array<FileProcessingFailure> };
-
-type IProcessFilesBase = {
-  state: State;
-  setScalar: Action<IProcessFilesBase, ScalarStatus>;
-  setFailed: Action<IProcessFilesBase, Array<FileProcessingFailure>>;
-  launch: Thunk<IProcessFilesBase>;
-  dismiss: Thunk<IProcessFilesBase>;
-};
-
-export type IProcessFilesInteraction<Specific = object> = IProcessFilesBase &
-  Specific & {
-    tryProcess: Thunk<
-      IProcessFilesBase & Specific,
-      FileList,
-      void,
-      IPytchAppModel
-    >;
-  };
-
-// This is a function returning a new object from a literal, rather than
-// a simple constant.  Turned out that using a constant here (and spread
-// into addAssetsInteraction and uploadZipfilesInteraction) resulted in
-// "addAssetsInteraction.launch()" launching the upload-zipfiles modal.
-// I didn't get to the bottom of this, but suspect it's to do with
-// Easy-Peasy needing some way to map from the thunk to its place in the
-// store, and this mechanism being confused if the self-same thunk
-// appears in two different places in the store.
-export const processFilesBase: () => IProcessFilesBase = () => ({
-  state: { status: "idle" },
-
-  setScalar: action((state, status) => {
-    state.state = { status };
-  }),
-
-  setFailed: action((state, failures) => {
-    state.state = {
-      status: "showing-failures",
-      failures,
-    };
-  }),
-
-  launch: thunk((actions) => actions.setScalar("awaiting-user-choice")),
-
-  dismiss: thunk((actions) => actions.setScalar("idle")),
-});
