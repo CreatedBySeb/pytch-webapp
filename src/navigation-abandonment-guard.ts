4a249197c6abb3e78b27a24b502037216d3ead68
Ben North <ben.north@tcd.ie>
2024-05-21 07:00:05 +0100

    Define ctor

--- src/navigation-abandonment-guard.ts
+++ src/navigation-abandonment-guard.ts
@@ -1,3 +1,5 @@
+import { promiseAndResolve } from "./utils";
+
 const kAbandonedError = new Error(
   "the user abandoned the operation by navigating backwards/forwards"
 );
@@ -5,4 +7,12 @@ const kAbandonedError = new Error(
 export class NavigationAbandonmentGuard {
   exit: () => void;
   abandoned: Promise<void>;
+
+  constructor() {
+    const { promise, resolve } = promiseAndResolve();
+    this.abandoned = promise;
+
+    window.addEventListener("popstate", resolve);
+    this.exit = () => window.removeEventListener("popstate", resolve);
+  }
 }
