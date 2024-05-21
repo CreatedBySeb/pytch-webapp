c3f5e56e5de1e569bdf69db2a5f664cd8f70f763
Ben North <ben.north@tcd.ie>
2024-05-21 06:59:47 +0100

    Define class skeleton

--- src/navigation-abandonment-guard.ts
+++ src/navigation-abandonment-guard.ts
@@ -1,3 +1,8 @@
 const kAbandonedError = new Error(
   "the user abandoned the operation by navigating backwards/forwards"
 );
+
+export class NavigationAbandonmentGuard {
+  exit: () => void;
+  abandoned: Promise<void>;
+}
