131410a3fc2e76de7a4dd0a221a82db5622045ec
Ben North <ben.north@tcd.ie>
2024-05-21 05:11:50 +0100

    InteractingAsyncUserFlowFsmState: Define type

--- src/model/user-interactions/async-user-flow.ts
+++ src/model/user-interactions/async-user-flow.ts
@@ -1,3 +1,10 @@
 type UserSettleResult = "cancel" | "submit";
 type UserSettleFun = (result: UserSettleResult) => void;
 const kIgnoreSettleResult: UserSettleFun = () => void 0;
+
+type InteractingAsyncUserFlowFsmState<RunStateT> = {
+  kind: "interacting";
+  maybeLastFailure: Error | null;
+  runState: RunStateT;
+  userSettle: UserSettleFun;
+};
