de214a46f395dc0dba7c734d97b723137f160e1e
Ben North <ben.north@tcd.ie>
2024-05-21 04:14:46 +0100

    (Reformat)

--- src/components/UploadZipfilesModal.tsx
+++ src/components/UploadZipfilesModal.tsx
@@ -12,43 +12,43 @@ export const UploadZipfilesModal = () => {
   const { setChosenFiles } = useFlowActions((f) => f.uploadZipfilesFlow);
 
   return asyncFlowModal(fsmState, (activeFsmState) => {
-  const { chosenFiles } = activeFsmState.runState;
-  const settle = settleFunctions(isSubmittable, activeFsmState);
+    const { chosenFiles } = activeFsmState.runState;
+    const settle = settleFunctions(isSubmittable, activeFsmState);
 
-  if (
-    activeFsmState.kind === "interacting" &&
-    activeFsmState.maybeLastFailure != null
-  ) {
-    const error = activeFsmState.maybeLastFailure as FileFailureError;
-    return (
-      <FileProcessingFailures
-        titleText="Problem uploading project zipfiles"
-        introText="Sorry, there was a problem uploading the projects:"
-        failures={error.fileFailures}
-        dismiss={settle.cancel}
-      />
-    );
-  }
-
-  switch (activeFsmState.kind) {
-    case "interacting":
-    case "attempting": {
+    if (
+      activeFsmState.kind === "interacting" &&
+      activeFsmState.maybeLastFailure != null
+    ) {
+      const error = activeFsmState.maybeLastFailure as FileFailureError;
       return (
-        <ChooseFiles
-          titleText="Upload project zipfiles"
-          introText="Choose zipfiles to upload as new projects."
-          actionButtonText="Upload"
-          status={activeFsmState.kind}
-          chosenFiles={chosenFiles}
-          setChosenFiles={setChosenFiles}
-          tryProcess={settle.submit}
+        <FileProcessingFailures
+          titleText="Problem uploading project zipfiles"
+          introText="Sorry, there was a problem uploading the projects:"
+          failures={error.fileFailures}
           dismiss={settle.cancel}
         />
       );
     }
-    case "succeeded":
-      // TODO: Something better here.
-      return <GenericWorkingModal />;
-  }
+
+    switch (activeFsmState.kind) {
+      case "interacting":
+      case "attempting": {
+        return (
+          <ChooseFiles
+            titleText="Upload project zipfiles"
+            introText="Choose zipfiles to upload as new projects."
+            actionButtonText="Upload"
+            status={activeFsmState.kind}
+            chosenFiles={chosenFiles}
+            setChosenFiles={setChosenFiles}
+            tryProcess={settle.submit}
+            dismiss={settle.cancel}
+          />
+        );
+      }
+      case "succeeded":
+        // TODO: Something better here.
+        return <GenericWorkingModal />;
+    }
   });
 };
