64621bd1fd0ef423b1db6446f2f6e655a7b24a25
Ben North <ben.north@tcd.ie>
2024-05-22 13:59:41 +0100

    Add skeleton context for e2e nav-abandonment tests

---
+++ cypress/e2e/no-stale-modals.cy.ts
@@ -0,0 +1,35 @@
+/// <reference types="cypress" />
+import {
+  createProjectFollowingTutorial,
+  initSpecimenIntercepts,
+  setInstantDelays,
+} from "./utils";
+
+context("Modals are cancelled when navigating away", () => {
+  ////////////////////////////////////////////////////////////////////////
+  // #region Common set-up
+
+  before(() => {
+    initSpecimenIntercepts();
+
+    cy.pytchResetDatabase();
+    cy.visit("/lesson/hello-world-lesson", { onLoad: setInstantDelays });
+    cy.pytchHomeFromIDE();
+
+    cy.get(".NavBar").contains("Tutorials").click();
+    createProjectFollowingTutorial("Catch the apple");
+    cy.pytchHomeFromIDE();
+
+    cy.pytchTryUploadZipfiles([
+      "newly-created-per-method.zip",
+      "simple-pytchjr-project.zip",
+    ]);
+
+    // We will now be at the "My projects" page.  Each test must ensure
+    // that it leaves the app at the "My projects" page.
+  });
+
+  beforeEach(initSpecimenIntercepts);
+
+  // #endregion
+});
