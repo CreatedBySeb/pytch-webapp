e6e688b1c8fd4bb73aa142b3b81680f20b1a817c
Ben North <ben.north@tcd.ie>
2024-08-29 17:27:10 +0100

    Add dynamic burger menu

--- src/components/NavBanner.tsx
+++ src/components/NavBanner.tsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { withinSite } from "../env-utils";
 import { Link } from "./LinkWithinApp";
 import { pytchResearchSiteUrl } from "../constants";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classNames from "classnames";
 
 import "../pytch-navbar.scss";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
 const NavBanner = () => {
+  const [menuIsExpanded, setMenuIsExpanded] = useState(false);
+  const menuRef = React.createRef<HTMLDivElement>();
+
+  useEffect(() => {
+    const menuDiv = menuRef.current;
+    if (menuDiv == null) return;
+
+    let resizeObserver: ResizeObserver | null = new ResizeObserver(() => {
+      const mMenuDisplay = menuDiv.computedStyleMap().get("display");
+      if (mMenuDisplay == null) return;
+
+      const menuDisplay = mMenuDisplay as CSSKeywordValue;
+      if (menuIsExpanded && menuDisplay.value === "none") {
+        setMenuIsExpanded(false);
+      }
+    });
+
+    resizeObserver.observe(menuDiv);
+
+    return () => {
+      resizeObserver?.disconnect();
+      resizeObserver = null;
+    };
+  });
+
+  const ulClass = classNames({ menuIsExpanded });
+  const toggleMenu = () => {
+    setMenuIsExpanded(!menuIsExpanded);
+  };
+
+  const burgerIcon: IconProp = menuIsExpanded ? "xmark" : "bars";
   // TODO: UL is supposed to directly contain LIs, so the below needs
   // restructuring.
   return (
@@ -15,7 +49,10 @@ const NavBanner = () => {
           <h1>Pytch</h1>
         </Link>
       </div>
-      <ul>
+      <div className="burger-menu" onClick={toggleMenu} ref={menuRef}>
+        <FontAwesomeIcon icon={burgerIcon} />
+      </div>
+      <ul className={ulClass}>
         <a href={pytchResearchSiteUrl}>
           <li>About</li>
         </a>
@@ -31,7 +68,7 @@ const NavBanner = () => {
         <Link to="/my-projects/">
           <li>My projects</li>
         </Link>
-        <Link to="/#contact-info">
+        <Link to="/#contact-info" onClick={() => setMenuIsExpanded(false)}>
           <li>
             <FontAwesomeIcon icon={["far", "envelope"]} />
           </li>
