c782b53f871713228926eb1fa0bce0d6a358abb3
Ben North <ben.north@tcd.ie>
2024-09-04 09:42:30 +0100

    launchCodingJourneyModal(): Define helper

--- cypress/e2e/front-page.cy.ts
+++ cypress/e2e/front-page.cy.ts
@@ -1,4 +1,11 @@
 /// <reference types="cypress" />
 
 context("Front page", () => {
+  type LaunchCodingJourneyModalOpts = { expectDeviceSizeWarning: boolean };
+  const launchCodingJourneyModal = (opts: LaunchCodingJourneyModalOpts) => {
+    cy.contains("coding journey").click();
+    cy.get(".modal").should("be.visible");
+    if (opts.expectDeviceSizeWarning)
+      cy.contains("reasonably large screen").should("be.visible");
+  };
 });
