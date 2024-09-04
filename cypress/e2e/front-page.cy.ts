25e0c4fff9f467fb9809a6db390ffef4fd78ba03
Ben North <ben.north@tcd.ie>
2024-09-04 09:43:08 +0100

    Define "narrow viewport" tests

--- cypress/e2e/front-page.cy.ts
+++ cypress/e2e/front-page.cy.ts
@@ -45,4 +45,56 @@ context("Front page", () => {
       cy.get(".StageControls");
       cy.go("back");
     });
+
+  context("narrow viewport", () => {
+    const burgerMenuShouldBe = (expState: "collapsed" | "expanded") => {
+      cy.get(`.burger-menu.is-${expState}`);
+    };
+    const openBurger = () => {
+      cy.get(".burger-menu.is-collapsed").click();
+      cy.get("li").contains("My projects").should("be.visible");
+      burgerMenuShouldBe("expanded");
+    };
+    const closeBurger = () => {
+      cy.get(".burger-menu.is-expanded").click();
+      cy.get("li").contains("My projects").should("not.be.visible");
+      burgerMenuShouldBe("collapsed");
+    };
+    const chooseBurgerItem = (match: string) => {
+      cy.get(".NavBar ul.menuIsExpanded li").contains(match).click();
+    };
+
+    beforeEach(() => {
+      cy.viewport(720, 960);
+      cy.visit("/");
+    });
+
+    it("looks OK", () => {
+      burgerMenuShouldBe("collapsed");
+      cy.get(".burger-menu").should("be.visible");
+      cy.get("li").contains("My projects").should("not.be.visible");
+    });
+
+    it("navigates to contact", () => {
+      openBurger();
+      cy.get("a.contact-us-link").click();
+      burgerMenuShouldBe("collapsed");
+      cy.contains("Please email us").should("be.visible");
+    });
+
+    it("burger menu", () => {
+      openBurger();
+      closeBurger();
+
+      openBurger();
+      chooseBurgerItem("My projects");
+      cy.location("href").should("eq", urlWithinBase("my-projects/"));
+      cy.get("h1").contains("My projects");
+      cy.go("back");
+
+      burgerMenuShouldBe("collapsed");
+    });
+
+    itRunsCodingJourneys({ expectDeviceSizeWarning: true });
+  });
 });
