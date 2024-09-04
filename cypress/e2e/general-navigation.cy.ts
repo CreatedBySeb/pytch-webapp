0bc7286408dbf3bc60fbc8ef4a633bb600bb8173
Ben North <ben.north@tcd.ie>
2024-09-04 13:25:36 +0100

    Remove obsolete e2e tests

--- cypress/e2e/general-navigation.cy.ts
+++ cypress/e2e/general-navigation.cy.ts
@@ -10,25 +10,6 @@ context("General site navigation", () => {
     assertOnHomepage();
   });
 
-  it("can navigate to My Projects way-of-using-pytch", () => {
-    cy.visit("/");
-    cy.get(".way-of-using-pytch")
-      .find("button")
-      .contains("My projects")
-      .click();
-    cy.get("h1").contains("My projects");
-    cy.get(".title-and-version h1").contains("Pytch").click();
-    cy.contains("bridge from Scratch to Python");
-  });
-
-  it("can navigate to Tutorials way-of-using-pytch", () => {
-    cy.visit("/");
-    cy.get(".way-of-using-pytch").find("button").contains("Tutorials").click();
-    cy.get("h1").contains("Tutorials");
-    cy.get(".title-and-version h1").contains("Pytch").click();
-    cy.contains("bridge from Scratch to Python");
-  });
-
   it("handles uncaught exceptions", () => {
     // In dev mode, React still thinks the exception is unhandled, so
     // tell it to ignore uncaught exceptions:
